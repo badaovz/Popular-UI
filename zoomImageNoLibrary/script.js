1a97151a9b4c0cb782fd08080cbc71d252878760
daonguyen <45008858+badaovz@users.noreply.github.com>
2023-01-28 16:21:05 +0700

    add zoom image effect

---
+++ zoomImageNoLibrary/script.js
@@ -0,0 +1,35 @@
+const image = document.querySelector('.image')
+const mirror = document.querySelector('.mirror')
+const container = document.querySelector('.container')
+
+
+function handleEventImage(e) {
+
+    const w = this.offsetWidth;
+    const h = this.offsetHeight;
+
+    const mouseWithBorderX = e.pageX - container.offsetLeft;
+    const mouseWithBorderY = e.pageY - container.offsetTop;
+
+    console.log(e.clientX, e.clientY);
+
+    const percentMouseByW = mouseWithBorderX/w * 100;
+    const percentMouseByH = mouseWithBorderY/h * 100;
+
+    console.log(percentMouseByW, percentMouseByH)
+    console.log('e: ', container.offsetTop)
+
+    mirror.style.backgroundPosition = `${percentMouseByW}% ${percentMouseByH}%`;
+    mirror.style.top = `${e.clientY}px`;
+    mirror.style.left = `${e.clientX}px`;
+    mirror.classList.add('show');
+    image.style.cursor = 'none';
+}
+
+function handleMouseOutImage(e) {
+    mirror.classList.remove('show');
+        
+}
+
+image.addEventListener('mousemove', handleEventImage.bind(image))
+container.addEventListener('mouseout', handleMouseOutImage)
