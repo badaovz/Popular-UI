3b964df78ec6514d627e7e78b7cad6264cce41af
daonguyen <45008858+badaovz@users.noreply.github.com>
2023-03-09 16:25:09 +0700

    update zoom-image-library

--- zoom-image-no-library/script.js
+++ zoom-image-no-library/script.js
@@ -1,9 +1,9 @@
-const image = document.querySelector('.image')
-const mirror = document.querySelector('.mirror')
-const container = document.querySelector('.container')
-
+const image = document.querySelector('.image');
+const mirror = document.querySelector('.mirror');
+const container = document.querySelector('.container');
 
 function handleEventImage(e) {
+    console.log('log message sss');
 
     const w = this.offsetWidth;
     const h = this.offsetHeight;
@@ -13,23 +13,22 @@ function handleEventImage(e) {
 
     console.log(e.clientX, e.clientY);
 
-    const percentMouseByW = mouseWithBorderX/w * 100;
-    const percentMouseByH = mouseWithBorderY/h * 100;
+    const percentMouseByW = (mouseWithBorderX / w) * 100;
+    const percentMouseByH = (mouseWithBorderY / h) * 100;
 
-    console.log(percentMouseByW, percentMouseByH)
-    console.log('e: ', container.offsetTop)
+    console.log(percentMouseByW, percentMouseByH);
+    console.log('e: ', container.offsetTop);
 
     mirror.style.backgroundPosition = `${percentMouseByW}% ${percentMouseByH}%`;
     mirror.style.top = `${e.clientY}px`;
     mirror.style.left = `${e.clientX}px`;
     mirror.classList.add('show');
-    image.style.cursor = 'none';
+    // image.style.cursor = 'none';
 }
 
 function handleMouseOutImage(e) {
     mirror.classList.remove('show');
-        
 }
 
-image.addEventListener('mousemove', handleEventImage.bind(image))
-container.addEventListener('mouseout', handleMouseOutImage)
+image.addEventListener('mousemove', handleEventImage.bind(image));
+container.addEventListener('mouseout', handleMouseOutImage);
